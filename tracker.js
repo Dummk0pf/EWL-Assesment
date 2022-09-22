58f54dd225ba435df65dd4877ecd5725ab1953df
Dummk0pf <110558786+Dummk0pf@users.noreply.github.com>
2022-09-22 15:11:43 +0530

    commit

--- tracker.js
+++ tracker.js
@@ -15,12 +15,26 @@ fetch("https://randomuser.me/api/?results=10")
 
 
 function display(data){
-    const imgv = data[0].picture.thumbnail;
-    
+    let card = document.getElementById("card1")
+    let imgv = data[0].picture.thumbnail;
+    let namev = data[0].name.first;
+    let emailv = data[0].email;
+    let genderv = data[0].gender;
+
+    let img = document.createElement("img");
+    img.src = imgv;
+    card.insertBefore(img,document.getElementById("card01"))
 
 
-    const namev = data[0].name.first;
-    const p = document.createElement("p");
+    let p = document.createElement("p");
     p.innerHTML = namev;
     document.getElementById("name").appendChild(p);
+
+    let p1 = document.createElement("p");
+    p1.innerHTML = emailv;
+    document.getElementById("email").appendChild(p1);
+
+    let p2 = document.createElement("p");
+    p2.innerHTML = genderv;
+    document.getElementById("gender").appendChild(p2);   
 }
